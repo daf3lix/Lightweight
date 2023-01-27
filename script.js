0d8023622c02b09cfa197f392ada8ecc06409094
daf3lix <121302346+daf3lix@users.noreply.github.com>
2023-01-27 20:27:02 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openTab(tabName) {
-    var i, elementList;
-    elementList = document.getElementsByClassName("content");
-    for (i = 0; i < elementList.length; i++) {
-    elementList[i].style.display = "none";
-    }   
-document.getElementById(tabName).style.display = "block";
-}
